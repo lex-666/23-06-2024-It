fc2d599f2651564e29e5b59de09aefd54dfd3ce9
Artem Zinchenko <lexart666@gmail.com>
2024-06-30 09:29:55 +0200

    12

--- app.js
+++ app.js
@@ -3,5 +3,15 @@ let array = [5];
 console.log(array);
 console.log("AAAAAAA");
 for(i=0;array[i]<array.length;i+1){
-    console.log(array[i]);
+    return console.log(array[i]);
 }
+console.log(123456)
+const art=567;
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
