71ac1db963c9478ee6a83636ad01d3728ab83db3
Artem Zinchenko <lexart666@gmail.com>
2024-07-03 09:57:01 +0200

    colors

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-console.log("hello world");
-let array = [5];
-console.log(array);
-console.log("AAAAAAA");
-for(i=0;array[i]<array.length;i+1){
-    return console.log(array[i]);
-}
-console.log(123456)
-const art=567;
-
-let result = ""
-
-for (const row of rows) {
-  result = result + "\n" + row;
-}
-
-console.log(result);
+const character = "!";
+const count = 10;
+const rows = [];
+let inverted = false;
+function padRow(rowNumber, rowCount) {
+    return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+  }
+  
+  for (let i = 1; i <= count; i++) {
+    if (inverted) {
+      rows.unshift(padRow(i, count));
+    } else {
+      rows.push(padRow(i, count));
+    }
+  }
+  
+  let result = ""
+  
+  for (const row of rows) {
+    result = result + "\n" + row;
+  }
+  
